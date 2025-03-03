157811ec7cf0674eac0437bebf1ece647e20d044
julienmalard <julien.malard@mail.mcgill.ca>
2025-03-03 08:18:24 +0100

    Add official default vars

--- src/consts.ts
+++ src/consts.ts
@@ -42,4 +42,24 @@ export const CONTENT_CATEGORIES = [
   "other",
 ];
 
-export const DEFAULT_VARIABLE_IDS = { }
+export const DEFAULT_VARIABLE_IDS = { 
+  trustedSitesSiteIdVar: "/orbitdb/zdpuAwapnjEwFwWSjXxzcvDypXXjfLGXSChHF6cUPgNUthEJf",
+  trustedSitesNameVar: "/orbitdb/zdpuAsuiXPa4go6gAnKmD33K18MLd4XTGTb5JhE1NyTaxuF25",
+  featuredReleasesReleaseIdVar: "/orbitdb/zdpuAxWTbRzgExEv8gyXqzqLSoQiTvtZ1a5jyp2xDrkE1wYTa",
+  featuredReleasesStartTimeVar: "/orbitdb/zdpuAtKvMTKcJoRNTczTK3dLVR3vMrP3eagFPUMbXJEkVxoLp",
+  featuredReleasesEndTimeVar: "/orbitdb/zdpuAnEuqR87b6ECMfAHHFw8yX4bgqNcNJefsKhvCWpmVzChX",
+  blockedReleasesReleaseIdVar: "/orbitdb/zdpuAyindpd8NqhQ4ao4ZpU8eFCUXkpCzn3A3jcF9P9YqEUnu",
+  releasesFileVar: "/orbitdb/zdpuAp5YGHgxsjs1QUJX2bYwLDK22V7Gf79m7SPeZhXy9LsiH",
+  releasesAuthorVar: "/orbitdb/zdpuAzqhnJQnoS7P1nbmdzJtQ8Vi4n2VLUWi1D7iPghvFkEm7",
+  releasesContentNameVar: "/orbitdb/zdpuB15wfgTPSazoxdbt5D9UMhTbnqkqBTXAo1uWe4F6B8514",
+  releasesThumbnailVar: "/orbitdb/zdpuAptC1ZQafFAqMoAqP1xPgNhQc8Rd3YCHLoq529d59RHek",
+  releasesCoverVar: "/orbitdb/zdpuAysVN5zt1utf4DqVS6w5L3SpAWK3TxxBe4pmnHHEquoVr",
+  releasesMetadataVar: "/orbitdb/zdpuArqWuk5ENQiUKUHYnqAP2dLApACKSLCtgY1hCK4AYRTHz",
+  releasesCategoryVar: "/orbitdb/zdpuAq1tNjCKNypDkdBU9ZxkjdR2xepYkDsTpm15MUyojgdfR",
+  collectionsAuthorVar: "/orbitdb/zdpuAtm5fMVa66nj2EW65bosvMZ2rsSvHgmPc8gUfzCVarxAL",
+  collectionsMetadataVar: "/orbitdb/zdpuB1c4D3BJu1rsNAbXJqaSZ8JL1QxswEiGHdnTAfQgWqGA9",
+  collectionsNameVar: "/orbitdb/zdpuAxJwMcwaZB9JQ6NkCUmKJYDuezuqWQjPfk7fxYBQZLorS",
+  collectionsThumbnailVar: "/orbitdb/zdpuB3DPsrNiifdTqhqUgvBCePqMYi1ftvHhq8mvuvDNvszcr",
+  collectionsReleasesVar: "/orbitdb/zdpuAu1KqKhQm5pXwPKqCvYFD7nRKmZaFHW19JuP7oBpYQeXm",
+  collectionsCategoryVar: "/orbitdb/zdpuAxhjVXv5gkjtDK8VoZ2jvst3LKCP97s7WuepTsyuyq1Xn"
+ }
