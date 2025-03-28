7a519a36e1347b468655dda018c36293609ddfd4
benya7 <bitup.games@gmail.com>
2025-03-28 16:31:28 +0100

    update: export configIsComplete util

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { Orbiter, createOrbiter, setUpSite } from "./orbiter.js";
+export { Orbiter, createOrbiter } from "./orbiter.js";
+export { configIsComplete } from "./config.js";
 export { version } from "./version.js";
 export * as types from "./types.js";
 export * as consts from "./consts.js";
