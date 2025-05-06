1b4a837cbe72a0f932a9d3d54b44fc38324492ec
benya7 <bitup.games@gmail.com>
2025-05-06 06:24:12 +0200

    fix: featuredReleasesPromotedVar value

--- src/consts.ts
+++ src/consts.ts
@@ -45,7 +45,7 @@ export const DEFAULT_VARIABLE_IDS = {
   featuredReleasesReleaseIdVar: "/orbitdb/zdpuAuoH7hoTZvBuAxG667k5YMSZaCQXU8Y88CoRG57vjZLaZ",
   featuredReleasesStartTimeVar: "/orbitdb/zdpuArQnMwqU1jN9eZ9J6k8H1euLRF67qVkeWjU9NBTAf4HLQ",
   featuredReleasesEndTimeVar: "/orbitdb/zdpuArhQVb8NYLXu8oyEbKZmdwGmPWXACxrYBR49zGkGyo2NV",
-  featuredReleasesPromotedVar: "/orbitdb/zdpuArhQVb8NYLXu8oyEbKZmdwGmPWXACxrYBR49zGkGyo2NV",
+  featuredReleasesPromotedVar: "/orbitdb/zdpuAzjr3v1PjM33JUp7NJpKtYMNeYQJwiR5K3REKeSCMoegq",
   blockedReleasesReleaseIdVar: "/orbitdb/zdpuAkyAA7XiPurjxZ39b2s5Vt57Z5dqnr6TSgMap5wP1qYBi",
   contentCategoriesCategoryIdVar: "/orbitdb/zdpuAtrNCjNSKf8hBM6rrqC4prVvsNph3Wt7E4Y2W8pFvGDXx",
   contentCategoriesDisplayNameVar: "/orbitdb/zdpuAzKVn6Ep8feK8uCtu9GFJGPK2Z3mEmPcg2STKAuGEsKdz",
